d2032ddf9a6a19740eb5bdfc7e74786201da6b17
Avi_saini <81501977+avisaini0501@users.noreply.github.com>
2022-05-19 18:24:25 +0530

    Add files via upload

---
+++ string,c/length_string.c
@@ -0,0 +1,18 @@
+//to find the length of strings
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+    char *string = "welcome";
+    int i;
+
+    for(i = 0; string[i] != '\0'; i++)
+    {
+
+    }
+
+    printf("length is %d\n",i);
+    return(0);
+}
